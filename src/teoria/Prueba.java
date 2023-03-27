d240b2efad1edd48bc7bbf24a036b114fdc14c4a
allamemc <allamemc@gmail.com>
2023-03-27 13:23:01 +0200

    a

--- src/teoria/Prueba.java
+++ src/teoria/Prueba.java
@@ -3,5 +3,12 @@ package teoria;
 public class Prueba {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        String nombre = "Allam Miranda";
+        char letra = 'd';
+        int unidades= 10;
+        float precio=9.95f;
+        float total=unidades*precio;
+        System.out.println("el total de la compra es "+total);
     }//cierra main
+
 }//cierra clase
