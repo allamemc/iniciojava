511128867dc6667ae20e8fb8c35a4d538a19113a
allamemc <allamemc@gmail.com>
2023-03-27 13:16:57 +0200

    -

--- src/teoria/Prueba.java
+++ src/teoria/Prueba.java
@@ -1,5 +1,7 @@
-public class PrimerosString {
+package teoria;
+
+public class Prueba {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-    }
-}
+    }//cierra main
+}//cierra clase
